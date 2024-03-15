740d82dfda289013b87ac493984b1e2c59f2ce69
Joslyn Manzi Karenzi <j.karenzi@alustudent.com>
2024-03-15 12:29:04 +0200

    chore(project-setup): Set up Mongoose and also project structure
    -set up empty express project with mongoose ORM
    
    [Finishes #lPxHIrzR]

---
+++ server.ts
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require("mongoose")
+require('dotenv').config()
+
+const app = express();
+app.use(express.json());
+
+const url = process.env.MONGO_URL
+const port = process.env.PORT
+
+mongoose.connect(url)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server listening at http://localhost:${port}`);
+        });
+    })
+    .catch((err:any) => {
+        console.log(err);
+    });
