d92a35f2d14ae1c67e8cd5f63f0257daaca5babf
Joslyn Manzi Karenzi <j.karenzi@alustudent.com>
2024-03-16 16:14:32 +0200

    refactor(query): data validation and status codes
    -add data validation with joi, to validate form data
    -refactor all routes to give back appropriate status codes depending on the action done by the route

---
+++ dist/ValidationSchema/QuerySchema.js
@@ -0,0 +1,9 @@
+"use strict";
+const Joi = require('joi');
+const createQueryValidationSchema = Joi.object({
+    userId: Joi.string().length(24).required(),
+    query: Joi.string().required()
+});
+module.exports = {
+    createQueryValidationSchema
+};
--- dist/controllers/QueryController.js
+++ dist/controllers/QueryController.js
@@ -3,9 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Query = require("../models/QueryModel");
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
+const { createQueryValidationSchema } = require("../ValidationSchema/QuerySchema");
 class queryController {
     static async createQuery(req, res) {
         const formData = req.body;
+        const validationResult = createQueryValidationSchema.validate(formData);
+        if (validationResult.error) {
+            return res.status(400).json({ msg: validationResult.error.details[0].message });
+        }
         const userId = formData.userId;
         const query = formData.query;
         const newQuery = Query({
@@ -14,7 +19,7 @@ class queryController {
         });
         try {
             await newQuery.save();
-            return res.json({ msg: "Query saved successfully" });
+            return res.status(201).json({ msg: "Query saved successfully" });
         }
         catch (err) {
             console.log(err);
@@ -24,7 +29,7 @@ class queryController {
     static async getQueries(req, res) {
         try {
             const queries = await Query.find();
-            return res.json({ queryList: queries });
+            return res.status(200).json({ queryList: queries });
         }
         catch (err) {
             return res.status(500).json({ msg: 'Internal server error' });
@@ -35,7 +40,7 @@ class queryController {
         try {
             const query = await Query.findOne({ _id: id });
             if (query) {
-                return res.json(query);
+                return res.status(200).json(query);
             }
             else {
                 return res.status(404).json({ msg: 'Query not found' });
