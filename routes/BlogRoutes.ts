611a602b1dbd51173af65f2fc16cb70d82fe0a4e
Joslyn Manzi Karenzi <j.karenzi@alustudent.com>
2024-03-19 18:58:27 +0200

    fix(import): fix import statements in routes
    -fix import statement that imports the controller file
    
    [Fixes #DsAmyoJf]

--- routes/BlogRoutes.ts
+++ routes/BlogRoutes.ts
@@ -1,22 +1,22 @@
 const express = require("express")
 import { Router } from 'express';
-const { createBlog, getBlogs, getBlog, updateBlog, deleteBlog, like, dislike } = require('../controllers/BlogController')
+const { blogController } = require('../controllers/BlogController')
 const { authorizeAdmin, authenticateUser } = require('../middleware/Authorization')
 
 const router: Router = express.Router()
 
-router.post('/create_blog', authenticateUser, authorizeAdmin, createBlog)
+router.post('/create_blog', authenticateUser, authorizeAdmin, blogController.createBlog)
 
-router.get('/get_blogs', getBlogs)
+router.get('/get_blogs', blogController.getBlogs)
 
-router.get('/get_blog/:id', getBlog)
+router.get('/get_blog/:id', blogController.getBlog)
 
-router.post('/update_blog', authenticateUser, authorizeAdmin, updateBlog)
+router.post('/update_blog', authenticateUser, authorizeAdmin, blogController.updateBlog)
 
-router.delete('/delete_blog/:id', authenticateUser, authorizeAdmin, deleteBlog)
+router.delete('/delete_blog/:id', authenticateUser, authorizeAdmin, blogController.deleteBlog)
 
-router.post('/like', authenticateUser, like)
+router.post('/like', authenticateUser, blogController.like)
 
-router.post('/dislike', authenticateUser, dislike)
+router.post('/dislike', authenticateUser, blogController.dislike)
 
 module.exports = router
