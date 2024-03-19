83628967e727f3eb828b1e340de23f704bada503
Joslyn Manzi Karenzi <j.karenzi@alustudent.com>
2024-03-19 14:45:54 +0200

    feat(user-mgt): Implement authentication and authorization middleware
    -add middleware for authenticating requests to protected api endpoints
    -add middleware for authenticating requests to admin endpoints
    
    [Delivers #OUmcobp9]

--- dist/routes/UserMgtRoutes.js
+++ dist/routes/UserMgtRoutes.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
-const { userMgtController } = require('../controllers/UserMgtController');
+const { getUsers, deleteUser, updateUsername, updatePassword, updateEmail } = require('../controllers/UserMgtController');
+const { authorizeAdmin, authenticateUser } = require('../middleware/Authorization');
 const router = express.Router();
-router.get('/get_users', userMgtController.getUsers);
-router.delete('/delete_user/:id', userMgtController.deleteUser);
-router.post('/update_username', userMgtController.updateUsername);
-router.post('/update_password', userMgtController.updatePassword);
-router.post('/update_email', userMgtController.updateEmail);
+router.get('/get_users', authenticateUser, authorizeAdmin, getUsers);
+router.delete('/delete_user/:id', authenticateUser, authorizeAdmin, deleteUser);
+router.post('/update_username', authenticateUser, updateUsername);
+router.post('/update_password', authenticateUser, updatePassword);
+router.post('/update_email', authenticateUser, updateEmail);
 module.exports = router;
--- routes/UserMgtRoutes.ts
+++ routes/UserMgtRoutes.ts
@@ -1,17 +1,18 @@
 const express = require("express")
 import { Router } from 'express';
-const { userMgtController } = require('../controllers/UserMgtController')
+const { getUsers, deleteUser, updateUsername, updatePassword, updateEmail } = require('../controllers/UserMgtController')
+const { authorizeAdmin, authenticateUser } = require('../middleware/Authorization')
 
 const router: Router = express.Router()
 
-router.get('/get_users', userMgtController.getUsers)
+router.get('/get_users', authenticateUser, authorizeAdmin, getUsers)
 
-router.delete('/delete_user/:id', userMgtController.deleteUser)
+router.delete('/delete_user/:id', authenticateUser, authorizeAdmin, deleteUser)
 
-router.post('/update_username', userMgtController.updateUsername)
+router.post('/update_username', authenticateUser, updateUsername)
 
-router.post('/update_password', userMgtController.updatePassword)
+router.post('/update_password', authenticateUser, updatePassword)
 
-router.post('/update_email', userMgtController.updateEmail)
+router.post('/update_email', authenticateUser, updateEmail)
 
 module.exports = router
