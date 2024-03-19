84d7037d51289396f2c005210cfb92b213e931af
Joslyn Manzi Karenzi <j.karenzi@alustudent.com>
2024-03-19 18:46:24 +0200

    fix(import): fix import statements in routes
    -fix import statement that imports the controller file
    
    [Fixes #DsAmyoJf]

--- dist/routes/QueryRoutes.js
+++ dist/routes/QueryRoutes.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
-const { createQuery, getQueries, getQuery } = require('../controllers/QueryController');
+const { queryController } = require('../controllers/QueryController');
 const { authorizeAdmin, authenticateUser } = require('../middleware/Authorization');
 const router = express.Router();
-router.post('/create_query', authenticateUser, authorizeAdmin, createQuery);
-router.get('/get_queries', authenticateUser, authorizeAdmin, getQueries);
-router.get('/get_query/:id', authenticateUser, authorizeAdmin, getQuery);
+router.post('/create_query', authenticateUser, authorizeAdmin, queryController.createQuery);
+router.get('/get_queries', authenticateUser, authorizeAdmin, queryController.getQueries);
+router.get('/get_query/:id', authenticateUser, authorizeAdmin, queryController.getQuery);
 module.exports = router;
--- routes/QueryRoutes.ts
+++ routes/QueryRoutes.ts
@@ -1,14 +1,14 @@
 const express = require("express")
 import { Router } from 'express';
-const { createQuery, getQueries, getQuery } = require('../controllers/QueryController')
+const { queryController } = require('../controllers/QueryController')
 const { authorizeAdmin, authenticateUser } = require('../middleware/Authorization')
 
 const router: Router = express.Router()
 
-router.post('/create_query', authenticateUser, authorizeAdmin, createQuery)
+router.post('/create_query', authenticateUser, authorizeAdmin, queryController.createQuery)
 
-router.get('/get_queries', authenticateUser, authorizeAdmin, getQueries)
+router.get('/get_queries', authenticateUser, authorizeAdmin, queryController.getQueries)
 
-router.get('/get_query/:id', authenticateUser, authorizeAdmin, getQuery)
+router.get('/get_query/:id', authenticateUser, authorizeAdmin, queryController.getQuery)
 
 module.exports = router
